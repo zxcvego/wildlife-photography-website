3c5810953464c7d23e38027e92fc36ee45e50013
zxcvego <zxcvego@gmail.com>
2022-09-25 23:10:15 +0200

    Navbar css improvements

--- wildlife-photography/src/components/Navbar/Navbar.tsx
+++ wildlife-photography/src/components/Navbar/Navbar.tsx
@@ -6,18 +6,16 @@ const Navbar = () => {
 	return (
 		<>
 			<div className="navbar-container">
-				<div className="navbar-flexbox">
-					<h1>John Doe</h1>
-					<div className="navbar-buttons-container">
-						<h2>HOME</h2>
-						<h2>ABOUT</h2>
-						<h2>GALLERY</h2>
-						<h2>PROJECTS</h2>
-						<h2>CONTACT</h2>
-						<h2 className="test">
-							<DensityMediumIcon />
-						</h2>
-					</div>
+				<h1>John Doe</h1>
+				<div className="navbar-buttons-container">
+					<h2>HOME</h2>
+					<h2>ABOUT</h2>
+					<h2>GALLERY</h2>
+					<h2>PROJECTS</h2>
+					<h2>CONTACT</h2>
+					<h2>
+						<DensityMediumIcon />
+					</h2>
 				</div>
 			</div>
 		</>
