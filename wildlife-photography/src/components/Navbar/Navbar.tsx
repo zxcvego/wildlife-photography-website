c268ef1e3e1c91b108a0b6f7b4dead1327069608
zxcvego <zxcvego@gmail.com>
2022-09-26 22:16:14 +0200

    Changed navbar to be responsive

--- wildlife-photography/src/components/Navbar/Navbar.tsx
+++ wildlife-photography/src/components/Navbar/Navbar.tsx
@@ -8,11 +8,13 @@ const Navbar = () => {
 			<div className="navbar-container">
 				<h1>John Doe</h1>
 				<div className="navbar-buttons-container">
-					<h2>HOME</h2>
-					<h2>ABOUT</h2>
-					<h2>GALLERY</h2>
-					<h2>PROJECTS</h2>
-					<h2>CONTACT</h2>
+					<div className="navbar-buttons-titles">
+						<h2>HOME</h2>
+						<h2>ABOUT</h2>
+						<h2>GALLERY</h2>
+						<h2>PROJECTS</h2>
+						<h2>CONTACT</h2>
+					</div>
 					<h2>
 						<MenuIcon />
 					</h2>
