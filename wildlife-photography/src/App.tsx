6d6d926a9f433347084197fac882d5b4fac1849f
zxcvego <zxcvego@gmail.com>
2022-09-27 22:39:07 +0200

    About me section

--- wildlife-photography/src/App.tsx
+++ wildlife-photography/src/App.tsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar/Navbar";
 import HomepageSlider from "./components/HomepageSlider/HomepageSlider";
+import AboutMe from "./components/AboutMe/AboutMe";
 
 const App = () => {
 	return (
 		<>
 			<Navbar />
 			<HomepageSlider />
+			<AboutMe />
 		</>
 	);
 };
---
+++ wildlife-photography/src/components/AboutMe/AboutMe.tsx
@@ -0,0 +1,28 @@
+import "./AboutMe.css";
+
+const AboutMe = () => {
+	return (
+		<>
+			<div className="about-me-decoration">
+				<div className="about-me-stripe"></div>
+				<h2 className="about-me-title">ABOUT ME</h2>
+			</div>
+
+			<div className="about-me-container">
+				<h1>Latio In The Counter To The Focus.</h1>
+				<h2>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Felis in nibh
+					in risus, cras tellus facilisis adipiscing. In massa at quis lorem vel
+					facilisis morbi. Id eget enim, leo sed nunc. Nisi praesent vitae
+					tincidunt iaculis a eget habitant sed. Eget cursus nisl ac, ut gravida
+					urna vel nascetur.
+					<br /> Semper quis gravida accumsan quam mattis tellus dignissim.
+					Commodo lectus in integer montes, consectetur sed nisl sollicitudin.
+					Sit sem sem odio mattis.
+				</h2>
+			</div>
+		</>
+	);
+};
+
+export default AboutMe;
