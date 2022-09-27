be3a38cc7caa286337dc41b9e8cc832aead62454
zxcvego <zxcvego@gmail.com>
2022-09-27 21:23:42 +0200

    Changing images on homepage slider

---
+++ wildlife-photography/src/components/HomepageSlider/CircleButton.tsx
@@ -0,0 +1,33 @@
+import "./CircleButton.css";
+import { sliderImagesI } from "./HomepageSlider";
+
+interface circleButtonI {
+	active: boolean;
+	id: number;
+	setSliderImageNumber: React.Dispatch<React.SetStateAction<number>>;
+	sliderImages: sliderImagesI[];
+	setSliderImages: React.Dispatch<React.SetStateAction<sliderImagesI[]>>;
+}
+
+const CircleButton = (props: circleButtonI) => {
+	const { active, id, setSliderImageNumber, sliderImages, setSliderImages } =
+		props;
+
+	return (
+		<>
+			<div
+				className={active ? "circle-button-filled" : "circle-button-unfilled"}
+				onClick={() => {
+					setSliderImageNumber(id);
+					const tempSliderImages = sliderImages;
+					tempSliderImages.forEach((x: any) => (x.active = false));
+					tempSliderImages[id].active = true;
+					setSliderImages(tempSliderImages);
+					console.log(tempSliderImages);
+				}}
+			></div>
+		</>
+	);
+};
+
+export default CircleButton;
--- wildlife-photography/src/components/HomepageSlider/HomepageSlider.tsx
+++ wildlife-photography/src/components/HomepageSlider/HomepageSlider.tsx
@@ -1,12 +1,27 @@
 import "./HomepageSlider.css";
-import "./CircleButton.css";
+import CircleButton from "./CircleButton";
 import nobleDeerImg from "../../assets/noble-deer.jpg";
+import tigerImg from "../../assets/tiger.jpg";
+import bearImg from "../../assets/bear.jpg";
+import { useState } from "react";
+
+export interface sliderImagesI {
+	image: string;
+	active: boolean;
+}
 
 const HomepageSlider = () => {
+	const [sliderImageNumber, setSliderImageNumber] = useState(0);
+	const [sliderImages, setSliderImages] = useState<sliderImagesI[]>([
+		{ image: nobleDeerImg, active: true },
+		{ image: tigerImg, active: false },
+		{ image: bearImg, active: false },
+	]);
+
 	return (
 		<>
 			<div className="homepage-slider-container">
-				<img src={nobleDeerImg} alt="noble deer" />
+				<img src={sliderImages[sliderImageNumber].image} alt="slider" />
 				<div className="slider-upper-title-container">
 					<div className="white-stripe"></div>
 					<h2 className="slider-upper-title">JOHN DOE</h2>
@@ -15,9 +30,30 @@ const HomepageSlider = () => {
 				<h1 className="slider-title">WILDLIFE PHOTOGRAPHER</h1>
 				<h2 className="slider-bottom-title">SINCE 2012</h2>
 				<div className="circle-button-container">
-					<div className="circle-button-filled"></div>
-					<div className="circle-button-unfilled"></div>
-					<div className="circle-button-unfilled"></div>
+					<CircleButton
+						key={0}
+						id={0}
+						active={sliderImages[0].active}
+						setSliderImageNumber={setSliderImageNumber}
+						sliderImages={sliderImages}
+						setSliderImages={setSliderImages}
+					/>
+					<CircleButton
+						key={1}
+						id={1}
+						active={sliderImages[1].active}
+						setSliderImageNumber={setSliderImageNumber}
+						sliderImages={sliderImages}
+						setSliderImages={setSliderImages}
+					/>
+					<CircleButton
+						key={2}
+						id={2}
+						active={sliderImages[2].active}
+						setSliderImageNumber={setSliderImageNumber}
+						sliderImages={sliderImages}
+						setSliderImages={setSliderImages}
+					/>
 				</div>
 			</div>
 		</>
