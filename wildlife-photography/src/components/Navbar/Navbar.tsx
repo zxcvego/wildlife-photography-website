acd86f317bb4c6b86f5df33ebd5645db8f06d621
zxcvego <zxcvego@gmail.com>
2022-10-10 23:33:13 +0200

    Expandable list from hamburger menu

--- wildlife-photography/src/components/Navbar/Navbar.tsx
+++ wildlife-photography/src/components/Navbar/Navbar.tsx
@@ -1,22 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import MenuIcon from "@mui/icons-material/Menu";
 
 const Navbar = () => {
+	const [isNavbarListMenuActive, setIsNavbarListMenuActive] = useState(false);
+
+	const navbarButtons = () => {
+		return (
+			<>
+				<h2>HOME</h2>
+				<h2>ABOUT</h2>
+				<h2>GALLERY</h2>
+				<h2>PROJECTS</h2>
+				<h2>CONTACT</h2>
+			</>
+		);
+	};
+
+	const expandNavbarListMenu = () => {
+		return (
+			<>
+				<div className="navbar-list">
+					<div className="navbar-buttons-list">{navbarButtons()}</div>
+				</div>
+				<div
+					className="dim"
+					onClick={() => setIsNavbarListMenuActive(!isNavbarListMenuActive)}
+				></div>
+			</>
+		);
+	};
+
 	return (
 		<>
 			<div className="navbar-container">
 				<h1>John Doe</h1>
 				<div className="navbar-buttons-container">
 					<div className="navbar-buttons-titles">
-						<h2>HOME</h2>
-						<h2>ABOUT</h2>
-						<h2>GALLERY</h2>
-						<h2>PROJECTS</h2>
-						<h2>CONTACT</h2>
+						{isNavbarListMenuActive ? navbarButtons() : expandNavbarListMenu()}
 					</div>
 					<h2 className="menu-icon">
-						<MenuIcon />
+						<MenuIcon
+							onClick={() => {
+								setIsNavbarListMenuActive(!isNavbarListMenuActive);
+								console.log(isNavbarListMenuActive);
+							}}
+						/>
 					</h2>
 				</div>
 			</div>
