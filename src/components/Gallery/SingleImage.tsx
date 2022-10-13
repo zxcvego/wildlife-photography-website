c984fa2efaea826d48c0a6c0888dbe61d9ba14cb
zxcvego <zxcvego@gmail.com>
2022-10-13 21:11:45 +0200

    Gallery image animation on hover

--- src/components/Gallery/SingleImage.tsx
+++ src/components/Gallery/SingleImage.tsx
@@ -9,7 +9,7 @@ const SingleImage = (props: imageDetailsI) => {
 	const { image, imageDescription } = props;
 
 	return (
-		<div className="image-wrapper">
+		<div className="image-wrapper animate__fadeIn">
 			<p className="image-description">{imageDescription}</p>
 			<img src={image} alt={imageDescription} />
 		</div>
